260157df75756b3db2981479ffc52dd2597852c2
Paul Murrell <paul@stat.auckland.ac.nz>
2024-07-29 09:59:58 +0100

    add "exercises.R" script from Heather

---
+++ DataVisWarwick/R/exercises.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(dplyr)
+
+# set working directory to directory of current file, e.g. in R Studio:
+path <- rstudioapi::getActiveDocumentContext()$path
+setwd(dirname(path))
+
+# source in crime data
+source("youth-crime.R")
+source("rwc.R")
